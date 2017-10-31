63448500ddbc7f3403a1f17667c27050076347ff
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-31 15:43:18 -0700

    Create ShakeDetector.java

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/ShakeDetector.java
@@ -0,0 +1,10 @@
+package edu.orangecoastcollege.cs273.magicanswer;
+
+import android.hardware.SensorEventListener;
+
+/**
+ * Created by sbadajoz on 10/31/2017.
+ */
+
+public class ShakeDetector implements SensorEventListener {
+}
