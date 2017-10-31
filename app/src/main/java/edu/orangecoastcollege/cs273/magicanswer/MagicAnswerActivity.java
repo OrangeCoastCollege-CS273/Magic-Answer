9013339b67b5910d0b414a58148c53f264d4551d
sbadajoz <sbadajoz@student.cccd.edu>
2017-10-31 16:48:17 -0700

    Make connections and create functionality

--- app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/magicanswer/MagicAnswerActivity.java
@@ -1,5 +1,8 @@
 package edu.orangecoastcollege.cs273.magicanswer;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -8,6 +11,9 @@ public class MagicAnswerActivity extends AppCompatActivity {
 
     MagicAnswer magicAnswer;
     private TextView answerTextView;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private ShakeDetector shakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +24,34 @@ public class MagicAnswerActivity extends AppCompatActivity {
         answerTextView = (TextView) findViewById(R.id.answerTextView);
 
         // TASK 2: CREATE A NEW MAGIC ANSWER OBJECT
+        magicAnswer = new MagicAnswer(this);
 
         // TASK 3: REGISTER THE SENSOR MANAGER AND SETUP THE SHAKE DETECTION
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        shakeDetector = new ShakeDetector(new ShakeDetector.OnShakeListener() {
+            @Override
+            public void onShake() {
+                displayMagicAnswer();
+            }
+        });
     }
 
+    public void displayMagicAnswer() {
+        String randomAnswer = magicAnswer.getRandomAnswer();
+        answerTextView.setText(randomAnswer);
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(shakeDetector, accelerometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(shakeDetector);
+    }
 }
